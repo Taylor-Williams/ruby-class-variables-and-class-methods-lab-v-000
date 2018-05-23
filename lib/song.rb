da5e49598fb8cb56c184eb36ddc6ffa3645f398c
Taylor Williams <taylor@thewilliams.net>
2018-05-23 19:49:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,12 @@ class Song
     genre_count
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      artist_count[artist] = 0 if !artist_count.keys.include?(artist)
+      artist_count[artist] += 1
+    end
+    artist_count
+  end
 end
