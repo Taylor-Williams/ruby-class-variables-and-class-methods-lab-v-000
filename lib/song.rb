6028d02846838ba96a82a48aa51b914aca73d4fb
Taylor Williams <taylor@thewilliams.net>
2018-05-23 19:42:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,22 +5,35 @@ class Song
   @@artists = []
 
   attr_accessor :name :artist :genre
-  
+
   def initialize(name, artist, genre)
     @name = song_name
     @artist = artist
     @genre = genre
     @@count += 1
     @@artists << artist if !@@artists.include?(artist)
-    @@genres << genre if !@@genres.inclue?(genre)
+    @@genres << genre
   end
 
-  def self.count
+  def count
     @@count
   end
 
+  def genres
+    @@genres.uniq
+  end
+
+  def artists
+    @@artists
+  end
+
   def genre_count
-    
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] = 0 if !genre_count.keys.include?(genre)
+      genre_count[genre] += 1
+    end
+    genre_count
   end
 
 end
